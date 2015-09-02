4311a3813f8eb7f2afe78bf10f3d6ddd363c5716
Shivan Kaul Sahib <shivankaul.1993@gmail.com>
2015-09-02 17:20:01 -0400

    Add description

--- repack.sh
+++ repack.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
+
+# WHAT:
+# Pack Chrome Extensions as zips for deploying on the Web Store.
+
+# HOW:
 # Keep this script in top-level of Chrome Extension project, right above contents of extension/ directory. 
 # It assumes that manifest.json file exists one level inside extension/ directory (as it should).
-# Removes .DS_Stores before packaging, if on Mac.
+
+# It checks the current version from the manifest file, and repacks the source code into a new version-numbered zip,
+# deleting the previous versioned zip.
+
+# Note: Removes .DS_Stores before packaging, if on Mac.
 
 file="extension/manifest.json"
 name=$(cat "$file" | grep "\"name\"" | cut -d ":" -f2 | cut -d "," -f1 | cut -d "\"" -f2)
