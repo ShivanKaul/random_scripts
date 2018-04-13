8ce191b0ddf1a09a30325cc8831e3f54dac16a37
Shivan Kaul Sahib <shivankaul.1993@gmail.com>
2018-04-13 15:16:17 -0400

    disk usage script

---
+++ disk_usage.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
+THRESHOLD=80
+
+if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
+    mail -s 'Disk Space Alert' ssahib@cisco.com << EOF
+    Your root partition remaining free space is critically low. Used: $CURRENT%
+EOF
+fi
